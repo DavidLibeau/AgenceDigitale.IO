670bd93f1c6eea2910e2ff80e14bd25c159a5861
DavidLibeau <davidl4@laposte.net>
2016-04-24 15:38:53 +0200

    Bug fixe and improvement (figcaption, backgroundfix...)

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-$("nav>i").click(function () {
+
+$("nav>i").click(function () { //Responsive Menu
     if ($("nav>div").css("visibility") == "hidden") {
         $("nav>div").css("visibility", "visible");
         $(this).removeClass("fa-bars").addClass("fa-times");
@@ -17,16 +18,24 @@ $("nav>i").click(function () {
     }
 });
 
-$("body").click(function (evt) {
+
+$("body").click(function (evt) { //Redirection
     evt.preventDefault();
     window.open("http://www.academie-francaise.fr/digital");
-}).contextmenu(function (evt) {
+}).contextmenu(function (evt) { //Right click to stop redirection
     if (redirection == true) {
         evt.preventDefault();
-        var r = confirm("Arrêter la redirection ?");
-        if (r == true) {
+        var reponseAlert = confirm("Arrêter la redirection ?");
+        if (reponseAlert == true) {
             $("body").off("click");
             redirection = false;
         }
+    }
+});
+
+
+$("[href]").click(function (evt) { //Desactivate redirection when it's an external link
+    if ($(this).attr("href")[0] != "#") {
+        evt.stopPropagation();
     }
 });
